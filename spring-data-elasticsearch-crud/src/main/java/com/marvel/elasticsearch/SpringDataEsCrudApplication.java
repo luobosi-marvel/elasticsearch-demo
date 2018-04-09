346471291083301499e398ef1cb3f28436f94eb4
luobosi <luobosi@2dfire.com>
2018-04-09 10:43:04 +0800

    spring-data-elasticsearch-crud 环境搭建

---
+++ spring-data-elasticsearch-crud/src/main/java/com/marvel/elasticsearch/SpringDataEsCrudApplication.java
@@ -0,0 +1,16 @@
+package com.marvel.elasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot 应用启动类
+ * @author marvel
+ */
+@SpringBootApplication
+public class SpringDataEsCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataEsCrudApplication.class, args);
+	}
+}
